85e38873cf94101ecbcc4f7c8d652254acb48b41
Князева Дарья <knyazevadasha2001@gmail.com>
2026-09-06 18:18:10 +0600

    исправила ошибки

--- homework-10.js
+++ homework-10.js
@@ -1,41 +1,34 @@
 import { products } from "./products.js";
 
-function createProductCard(product) {
-  return `
-    <li class="products__item card">
-      <img src="${product.image}" alt="Товар - ${product.name}" class="card__image" />
-      <span class="card__category">${product.category}</span>
-      <h2 class="card__name">${product.name}</h2>
-      <p class="card__description">${product.description}</p>
-
-      <div class="card__compound compound">
-        <span class="compound__name">Состав:</span>
-        <ul class="compound__list">
-          ${product.compound.map((item) => `<li>${item}</li>`).join("")}
-        </ul>
-      </div>
-
-      <div class="card__price">
-        <b>Цена</b>
-        <span>${product.price}</span>
-      </div>
-    </li>
-  `;
-}
+const productTemplate = document.querySelector(".product-template");
+const productList = document.getElementById("products-list");
 
 function renderProducts(productsArray) {
-  const list = document.getElementById("products-list");
-  list.innerHTML += productsArray
-    .map((product) => createProductCard(product))
-    .join("");
+  productsArray.forEach((product) => {
+    const productClone = productTemplate.content.cloneNode(true);
+
+    const image = productClone.querySelector(".card__image");
+    image.src = product.image;
+    image.alt = "Товар - " + product.name;
+
+    productClone.querySelector(".card__category").textContent =
+      product.category;
+    productClone.querySelector(".card__name").textContent = product.name;
+    productClone.querySelector(".card__description").textContent =
+      product.description;
+
+    const compoundItems = productClone.querySelectorAll(".compound__item");
+    compoundItems.forEach((item, index) => {
+      item.textContent = product.compound[index];
+    });
+
+    productClone.querySelector(".card__price-value").textContent =
+      product.price;
+
+    productList.appendChild(productClone);
+  });
 }
 
-const productDescriptions = products.reduce((acc, product) => {
-  acc[product.name] = product.description;
-  return acc;
-}, {});
-
-console.log(productDescriptions);
 function getCardsCount() {
   const input = prompt("Сколько карточек отобразить? От 1 до 5");
   const count = Number(input);
@@ -47,7 +40,15 @@ function getCardsCount() {
 
   return count;
 }
+
 const cardsCount = getCardsCount();
 const productsToShow = products.slice(0, cardsCount);
 
 renderProducts(productsToShow);
+
+const productDescriptions = products.reduce((acc, product) => {
+  acc[product.name] = product.description;
+  return acc;
+}, {});
+
+console.log(productDescriptions);
